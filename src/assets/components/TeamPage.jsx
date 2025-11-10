33ae8cb8aa5e3630b8a407188543262edf795af3
Your GitHub Username <youremail@example.com>
2025-11-10 21:44:23 +0100

    correction

--- src/assets/components/TeamPage.jsx
+++ src/assets/components/TeamPage.jsx
@@ -1,7 +1,7 @@
 // pages/TeamPage.jsx
 import React from 'react';
 import TeamShowcase from '../components/TeamShowcase';
-import Header from '../components/Header';
+
 
 const TeamPage = () => {
   return (
